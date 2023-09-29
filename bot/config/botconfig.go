caedf60cdf706bf09da8ede256910e8b4a41b9fa
GodOfPro <1387ilia@gmail.com>
2023-09-29 15:52:53 +0330

    Add GuildId to botconfig

--- bot/config/botconfig.go
+++ bot/config/botconfig.go
@@ -3,9 +3,13 @@ package config
 import "os"
 
 type BotConfig struct {
-	Token string
+	Token   string
+	GuildId string
 }
 
 func NewBotConfig() BotConfig {
-	return BotConfig{Token: os.Getenv("BOT_TOKEN")}
+	return BotConfig{
+		Token:   os.Getenv("BOT_TOKEN"),
+		GuildId: os.Getenv("GUILD_ID"),
+	}
 }
