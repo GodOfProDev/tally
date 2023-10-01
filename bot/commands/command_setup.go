22940eba133a47a2ff45dbfceed8c677e3ccf83e
GodOfPro <1387ilia@gmail.com>
2023-10-01 22:10:21 +0330

    Some basic functionality added

--- bot/commands/command_setup.go
+++ bot/commands/command_setup.go
@@ -6,10 +6,38 @@ import (
 )
 
 func (c *Commands) ExecuteSetupCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	options := i.ApplicationCommandData().Options
+
+	if len(options) != 1 {
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Invalid options provided",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+
+		return
+	}
+
+	option := options[0]
+
+	if option.Name != "count-channel" {
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Invalid options provided",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		return
+	}
+
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Content: "Hey there! Setup",
+			Content: "Hey there! Setup " + option.ChannelValue(c.app).ID,
+			Flags:   discordgo.MessageFlagsEphemeral,
 		},
 	})
 }
@@ -18,7 +46,18 @@ func (c *Commands) RegisterSetupCommand() {
 
 	command := discordgo.ApplicationCommand{
 		Name:        "setup",
-		Description: "Responds with setup",
+		Description: "Setup the counting channel",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionChannel,
+				Name:        "count-channel",
+				Description: "The channel it will use as it counting channel",
+				ChannelTypes: []discordgo.ChannelType{
+					discordgo.ChannelTypeGuildText,
+				},
+				Required: true,
+			},
+		},
 	}
 
 	_, err := c.app.ApplicationCommandCreate(c.app.State.User.ID, c.config.GuildId, &command)
